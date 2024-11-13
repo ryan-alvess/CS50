57fb53eb04eb9d3cf0d992bc449ebc1344d9eb4d
Ryan Alves <ryanalvesdelima2@gmail.com>
2024-11-13 14:43:15 -0300

    add squat counter exercise

---
+++ squat.c
@@ -0,0 +1,21 @@
+/*
+      Program that count squats
+
+    Author: Ryan Alves
+    Course: CS50 - 2024 - Week 1
+*/
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int counter = 0;
+    while (counter < 6)
+    {
+        printf("Agachamento \n");
+        counter++;
+    }
+
+}
+
